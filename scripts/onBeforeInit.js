ce31232e4a1a0db93e2030ab753f43dd5fc5bbab
sych74 <sych74@gmail.com>
2023-02-14 08:56:07 +0100

    WP-249

--- scripts/onBeforeInit.js
+++ scripts/onBeforeInit.js
@@ -11,7 +11,7 @@ var cdnAppid = "c05ffa5b45628a2a0c95467ebca8a0b4",
 function defineAppFields(appid, name) {
     resp = jelastic.dev.apps.GetApp(appid);
 
-    if (resp.result != 0 || resp.result != Response.PERMISSION_DENIED) {
+    if (!(resp.result == 0 || resp.result == Response.PERMISSION_DENIED)) {
         fields[name].hidden = true;
         fields[name].value = false;
     }
